3b88809be0961f75f890c68b1a8e599dd9974a1b
vishwajeeetmandwe <vishwajeetmandwe@gmail.com>
2019-08-12 15:29:35 +0530

    Forms

--- angular-workspace/projects/routers/src/app/blog/blog-page/blog-page.component.ts
+++ angular-workspace/projects/routers/src/app/blog/blog-page/blog-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Reg } from "./reg";
 
 @Component({
-  selector: 'app-blog-page',
-  templateUrl: './blog-page.component.html',
-  styleUrls: ['./blog-page.component.css']
+  selector: "app-blog-page",
+  templateUrl: "./blog-page.component.html",
+  styleUrls: ["./blog-page.component.css"]
 })
 export class BlogPageComponent implements OnInit {
+  fg: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-  }
+    let  emlVal = Validators.compose([Validators.required,
+      Validators.email])
 
+    this.fg = this.fb.group({
+      usNm: this.fb.control("",Validators.required),
+      eml: this.fb.control("", emlVal ),
+      pass: this.fb.control("",Validators.required)
+    });
+  }
+ 
+  onSub() {
+    console.log(this.fg);
+
+    let reg = this.fg.value as Reg;
+  }
 }
--- angular-workspace/projects/routers/src/app/blog/blog.module.ts
+++ angular-workspace/projects/routers/src/app/blog/blog.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogPageComponent } from './blog-page/blog-page.component';
-
+import { ReactiveFormsModule} from '@angular/forms'
 
 
 @NgModule({
   declarations: [BlogPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports : [BlogPageComponent]
 })
